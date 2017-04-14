a6b3c85aea68970970f2f9a4a31e71a0ddc72c47
Reggie Granados <reggie.granados@swirl.net>
2017-04-14 11:55:10 -0400

    Initial commit for lightweight reddit clone

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'My Reddit Client';
+  subreddit: string = '/r/askreddit';
+
+  updateSubreddit = function(subReddit) {
+    this.subreddit = subReddit;
+  }
+
+  getSubredditByList = function(eventStr) {
+    this.updateSubreddit(eventStr.target.dataset.subreddit);
+  }
+
+  getSubredditByValue = function(e) {
+    this.updateSubreddit(e.target.value);
+  }
+}
---
+++ src/app/my-reddit/my-reddit.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MyDataService } from '../my-data.service';
+import { Post } from '../post';
+
+@Component({
+  selector: 'app-my-reddit',
+  templateUrl: './my-reddit.component.html',
+  styleUrls: ['./my-reddit.component.css'],
+  providers: [MyDataService]
+})
+export class MyRedditComponent implements OnInit {
+  @Input('subreddit') subreddit: string;
+
+  posts: Post[];
+
+  constructor(private data: MyDataService) { }
+
+  ngOnInit() {
+    this.updateSubreddit();
+  }
+
+  ngOnChanges() {
+    this.updateSubreddit();
+  }
+
+  updateSubreddit = function() {
+    console.log('Fetching posts for ' + this.subreddit);
+    this.data.fetchPosts(this.subreddit).subscribe(
+      posts => this.posts = posts
+    );
+  }
+
+}
